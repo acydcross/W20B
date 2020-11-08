54717938b8a6e53add5a7e91cd0a914e64c03d07
acydcross <acydcross@hotmail.com>
2020-11-07 22:25:11 -0700

    Created function.py

---
+++ function.py
@@ -0,0 +1,21 @@
+import mariadb
+import dbcreds
+
+def signUp(username, password):
+    conn = None
+    cursor = None
+    try:
+        conn = mariadb.connect(user=dbcreds.user, password=dbcreds.password, host=dbcreds.host, port=dbcreds.port, database=dbcreds.database)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO hackers (alias, password) VALUES (?,?)", [username, password])
+        conn.commit()
+        if cursor.rowcount == 1:
+            print("A hacker is created!")
+        else:
+            print("Failed to create a hacker!")
+        finally:
+            if(cursor != None):
+                cursor.close()
+            if(conn != None):
+                conn.rollback()
+                conn.close()
